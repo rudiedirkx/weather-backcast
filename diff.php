908d56b22e5f9a6938ee562810c57785d3544d7d
Rudie Dirkx <github@hotblocks.nl>
2018-08-17 15:49:39 +0200

    +hours ahead diff

---
+++ diff.php
@@ -0,0 +1,56 @@
+<?php
+
+use rdx\weather\Prediction;
+
+require __DIR__ . '/inc.bootstrap.php';
+
+$measurements = $db->fetch("
+	select
+		hours_ahead,
+		round(avg(tempdiff), 1) tempdiff,
+		round(avg(sundiff), 1) sundiff,
+		round(avg(raindiff), 1) raindiff,
+		sum(measurements) measurements
+	from (
+		select
+			a.predicted_for,
+			a.taken_on,
+			round((a.predicted_for - a.taken_on) / 3600) hours_ahead,
+			count(1) measurements,
+			round(max(b.temperature) - min(b.temperature), 1) tempdiff,
+			round(max(b.sunshine) - min(b.sunshine), 1) sundiff,
+			round(max(b.precipitation) - min(b.precipitation), 1) raindiff
+		from predictions a
+		join predictions b on b.predicted_for = a.predicted_for and b.taken_on > a.taken_on
+		group by a.predicted_for, a.taken_on, hours_ahead
+		having measurements > 1
+	) x
+	group by hours_ahead
+	order by hours_ahead desc
+");
+
+header('Content-type: text/html; charset=utf-8');
+
+?>
+<table border="1" cellspacing="0" cellpadding="6">
+	<thead>
+		<tr>
+			<th>Hours ahead</th>
+			<th>Avg temp diff</th>
+			<th>Avg sun diff</th>
+			<th>Avg rain diff</th>
+			<th align="right"># measured</th>
+		</tr>
+	</thead>
+	<tbody>
+		<? foreach ($measurements as $measurement): ?>
+			<tr>
+				<td><?= number_format($measurement->hours_ahead, 0) ?></td>
+				<td><?= number_format($measurement->tempdiff, 1) ?></td>
+				<td><?= number_format($measurement->sundiff, 1) ?></td>
+				<td><?= number_format($measurement->raindiff, 1) ?></td>
+				<td align="right"><?= number_format($measurement->measurements, 0) ?></td>
+			</tr>
+		<? endforeach ?>
+	</tbody>
+</table>
---
+++ raw.php
@@ -0,0 +1,46 @@
+<?php
+
+use rdx\weather\Prediction;
+
+require __DIR__ . '/inc.bootstrap.php';
+
+$measurements = $db->fetch("
+	select
+		strftime('%Y-%m-%d %H:00', datetime(predicted_for, 'unixepoch')) hour,
+		count(1) measurements,
+		max(temperature) max,
+		min(temperature) min,
+		(max(temperature) - min(temperature)) diff,
+		((max(taken_on) - min(taken_on)) / 3600) timespan
+	from predictions
+	group by hour
+	order by hour
+");
+
+header('Content-type: text/html; charset=utf-8');
+
+?>
+<table border="1" cellspacing="0" cellpadding="6">
+	<thead>
+		<tr>
+			<th>Datetime</th>
+			<th>Max</th>
+			<th>Min</th>
+			<th>Diff</th>
+			<th align="right"># measured</th>
+			<th>In # hours</th>
+		</tr>
+	</thead>
+	<tbody>
+		<? foreach ($measurements as $measurement): ?>
+			<tr>
+				<td><?= $measurement->hour ?></td>
+				<td><?= number_format($measurement->max, 1) ?></td>
+				<td><?= number_format($measurement->min, 1) ?></td>
+				<td><?= number_format($measurement->diff, 1) ?></td>
+				<td align="right"><?= $measurement->measurements ?></td>
+				<td><?= (round($measurement->timespan) + 1) ?></td>
+			</tr>
+		<? endforeach ?>
+	</tbody>
+</table>
